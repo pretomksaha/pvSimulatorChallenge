d20bbcb9cf37b704877f1afbd4cd8de484f5b070
pretomksaha <pretomksaha@yahoo.com>
2022-05-05 21:19:09 +0200

    create meter to generate power and publish

--- meter.py
+++ meter.py
@@ -1,12 +1,39 @@
+import random
+
 import pika
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost')
-)
 
-channel = connection.channel()
-channel.queue_declare(queue = 'hello')
+class Meter():
 
-channel.basic_publish(exchange='',routing_key='hello',body="Hello World")
+    def __init__(self):
+        self.brokeHost = 'localhost'
+        self.brokePort = ''
+        self.brokeCredentials = pika.PlainCredentials('', '')
+        self.brokerQueue=''
+        self.minWatt=0
+        self.maxWatt=9000
 
-channel.close()
+
+    def connection(self):
+        try:
+            connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host='localhost')
+            )
+
+            channel = connection.channel()
+            channel.queue_declare(queue='hello')
+            channel.confirm_delivery()
+            powerValue= self.powerGenerate()
+            self.publishMassage(channel,powerValue)
+        except pika.exceptions.ConnectionClosedByBroker:
+            print("The connection  is terminated by Broker.")
+
+    def powerGenerate(self):
+        generatePower=random.uniform(self.minWatt,self.maxWatt)
+        return generatePower
+
+
+    def publishMassage(channel,powerValue):
+        channel.basic_publish(exchange='',routing_key='hello',body=f"Hello World{powerValue}")
+
+        channel.close()
--- pvsimulator.py
+++ pvsimulator.py
@@ -1,5 +1,6 @@
 import pika
 
+
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost')
 )
